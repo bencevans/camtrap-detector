55fc36ee6741bc9600f305fcccf9235c5c181227
Ben Evans <ben@bluechimp.io>
2022-08-12 11:46:03 +0100

    opencv in repo

---
+++ build-opencv.sh
@@ -0,0 +1,1 @@
+mkdir -p src-tauri/vendor/opencv/build && cd src-tauri/vendor/opencv/build && rm -rf ./* && cmake -DBUILD_SHARED_LIBS=off -DBUILD_LIST=core,dnn,imgcodecs -DCMAKE_INSTALL_PREFIX=install -DCMAKE_BUILD_TYPE=RELEASE -DWITH_OPENEXR=off -DWITH_TIFF=off -DWITH_WEBP=off -DWITH_OPENJPEG=off -DWITH_JASPER=off ..  && cmake --build . -j 8 && make install
