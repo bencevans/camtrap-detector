4cb4c77b46f8eb86a7196dc8570933af4d549653
Ben Evans <ben@bluechimp.io>
2022-09-20 15:22:28 +0100

    always use builtin

--- src-tauri/src/megadetector.rs
+++ src-tauri/src/megadetector.rs
@@ -1,25 +1,10 @@
 use yolov5cv::YoloModel;
 
-const MODEL_PATH: &str = "/Users/ben/Projects/camtrap-detector/md_v5a.0.0.onnx";
-const MODEL_INPUT_SIZE: (i32, i32) = (640, 640);
-
 pub fn load_model() -> YoloModel {
-  #[cfg(feature = "builtin")]
-  {
-      let model_vector: opencv::core::Vector<u8> = include_bytes!("/Users/ben/Projects/camtrap-detector/md_v5a.0.0.onnx")
-          .iter()
-          .cloned()
-          .collect();
+    let model_vector: opencv::core::Vector<u8> = include_bytes!("../../md_v5a.0.0.onnx")
+        .iter()
+        .cloned()
+        .collect();
 
-      YoloModel::new_from_buffer(&model_vector, (640, 640)).unwrap()
-  }
-
-  #[cfg(not(feature = "builtin"))]
-  {
-      YoloModel::new_from_file(
-          MODEL_PATH,
-          MODEL_INPUT_SIZE,
-      )
-      .unwrap()
-  }
+    YoloModel::new_from_buffer(&model_vector, (640, 640)).unwrap()
 }
