f3e2544c7391a7f7ffac2b1e97c8ff3a3014121a
Ben Evans <ben@bluechimp.io>
2022-12-12 22:43:01 +0000

    Run CI on Hosted Runners (#72)
    * Run CI on Hosted Runners
    
    * Windows + Linux
    
    * Rename workflow
    
    * System Deps
    
    * rm ittnotify
    
    * let lib sort includes
    
    * bash
    
    * linux deps
    
    * non escape
    
    * rm vendored opencv
    
    * specify triplet
    
    * disable binary cache
    
    * system vcpkg

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,15 +1,4 @@
-use std::env::var;
-
 fn main() {
-    let manifest_dir = var("CARGO_MANIFEST_DIR").unwrap();
-
-    println!("cargo:rustc-link-search={}/vendor/opencv/build/3rdparty/lib", manifest_dir);
-    println!("cargo:rustc-link-lib=ittnotify");
-    println!("cargo:rustc-link-lib=libjpeg-turbo");
-    println!("cargo:rustc-link-lib=libpng");
-    println!("cargo:rustc-link-lib=libprotobuf");
-    println!("cargo:rustc-link-lib=zlib");
-
     if cfg!(target_arch = "aarch64") {
         println!("cargo:rustc-link-lib=tegra_hal");
     }
