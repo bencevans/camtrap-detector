f448d549b2462c820a2de07fad28aad53cc09100
Ben Evans <ben@bluechimp.io>
2022-12-30 14:01:21 +0000

    Remove builtin and add cuda features

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,12 +1,3 @@
 fn main() {
-    if cfg!(feature = "cuda") {
-        let cuda_path = std::env::var("CUDA_PATH").expect("CUDA_PATH not set");
-        println!("cargo:rustc-link-search=native={}\\lib\\x64", cuda_path);
-        println!("cargo:rustc-link-lib=static=cudart_static");
-        println!("cargo:rustc-link-lib=static=cublas");
-        println!("cargo:rustc-link-lib=static=cublasLt");
-        println!("cargo:rustc-link-lib=static=cudnn");
-    }
-
     tauri_build::build()
 }
