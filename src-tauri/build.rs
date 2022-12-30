285d838755d462db2d8a7078761beb0bdd947de1
Ben Evans <ben@bluechimp.io>
2022-12-30 13:46:25 +0000

    Remove libs from build.rs
    Covered by build.rs in rust-opencv-yolov5

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -8,14 +8,5 @@ fn main() {
         println!("cargo:rustc-link-lib=static=cudnn");
     }
 
-    if cfg!(target_arch = "aarch64") {
-        println!("cargo:rustc-link-lib=tegra_hal");
-    }
-
-    if cfg!(target_os = "macos") {
-        println!("cargo:rustc-link-lib=framework=OpenCL");
-        println!("cargo:rustc-link-lib=framework=Accelerate");
-    }
-
     tauri_build::build()
 }
