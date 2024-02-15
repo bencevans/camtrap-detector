95ed7d6cd58980e277a55c662f4baef3093c04ea
Ben Evans <ben@bluechimp.io>
2024-02-15 23:10:32 +0000

    wip

--- src-tauri/src/yolov5/model.rs
+++ src-tauri/src/yolov5/model.rs
@@ -1,13 +1,11 @@
-use image::Rgb;
-use ndarray::CowArray;
-use ort::{ExecutionProvider, GraphOptimizationLevel, Session};
-
 use super::{YoloDetection, YoloImageDetections};
+use image::{buffer::ConvertBuffer, GenericImageView, ImageBuffer, RgbImage, RgbaImage};
+use ndarray::CowArray;
+use ort::{ExecutionProvider, Session};
 
 /// A YOLO model.
 pub struct YoloModel {
     session: Session,
-    // environment: Arc<Environment>,
     input_size: (u32, u32),
 }
 
@@ -17,12 +15,6 @@ impl YoloModel {
         path: &str,
         input_size: (u32, u32),
     ) -> Result<Self, Box<dyn std::error::Error>> {
-        // tracing_subscriber::fmt::init();
-        // let dylib_path = crate::internal::find_onnxruntime_dylib()?; // /etc/.../libonnxruntime.so
-        // println!("dylib_path: {:?}", dylib_path);
-
-        // ort::init_from(dylib_path).commit()?;
-
         let coreml = ort::CoreMLExecutionProvider::default();
         if !coreml.is_available().unwrap() {
             eprintln!("Please compile ONNX Runtime with CoreML!");
@@ -32,20 +24,20 @@ impl YoloModel {
 
         let session = ort::Session::builder()?
             .with_execution_providers([
-                // // Prefer TensorRT over CUDA.
-                ort::TensorRTExecutionProvider::default().build(),
-                ort::CUDAExecutionProvider::default().build(),
-                // Use DirectML on Windows if NVIDIA EPs are not available
-                ort::DirectMLExecutionProvider::default().build(),
-                ort::CoreMLExecutionProvider::default()
-                    // Or use ANE on Apple platforms
-                    .with_subgraphs()
-                    // only use the ANE as the CoreML CPU implementation is super slow for this model
-                    // .with_ane_only()
-                    .build(),
+                // // // Prefer TensorRT over CUDA.
+                // ort::TensorRTExecutionProvider::default().build(),
+                // ort::CUDAExecutionProvider::default().build(),
+                // // Use DirectML on Windows if NVIDIA EPs are not available
+                // ort::DirectMLExecutionProvider::default().build(),
+                // ort::CoreMLExecutionProvider::default()
+                //     // Or use ANE on Apple platforms
+                //     .with_subgraphs()
+                //     // only use the ANE as the CoreML CPU implementation is super slow for this model
+                //     // .with_ane_only()
+                //     .build(),
             ])
             .unwrap()
-            .with_optimization_level(GraphOptimizationLevel::Level3)?
+            // .with_optimization_level(GraphOptimizationLevel::Level3)?
             // .with_intra_threads(4)?
             .with_model_from_file(path)?;
 
@@ -66,116 +58,170 @@ impl YoloModel {
     ) -> Result<YoloImageDetections, Box<dyn std::error::Error>> {
         println!("Warning: YoloModel::detect is not implemented");
 
-        let time = std::time::Instant::now();
-        println!("Loading image...");
-        let image = image::open(image_path).unwrap();
-        println!("Loaded image: {:?}", time.elapsed());
+        // Load the image
+        let (original_image, original_image_width, original_image_height) = {
+            let time = std::time::Instant::now();
+            println!("Loading image...");
+            let original_image = image::open(image_path).unwrap();
+            let original_image_width = original_image.width();
+            let original_image_height = original_image.height();
 
-        // TODO: Letterboxing
+            let image = original_image;
+            println!("Loaded image: {:?}", time.elapsed());
+            (image, original_image_width, original_image_height)
+        };
 
-        let time = std::time::Instant::now();
-        println!("Resizing image...");
-        let x: image::ImageBuffer<image::Rgba<u8>, Vec<u8>> = image::imageops::resize(
-            &image,
-            self.input_size.0,
-            self.input_size.1,
-            image::imageops::FilterType::Nearest,
-        );
-        println!("Resized image: {:?}", time.elapsed());
+        let image = {
+            // 1. Letterbox the image
 
-        let time = std::time::Instant::now();
-        println!("Converting image...");
-        let mut output: image::ImageBuffer<Rgb<u8>, Vec<_>> =
-            image::ImageBuffer::new(self.input_size.0, self.input_size.1);
-        for (output, chunk) in output.chunks_exact_mut(3).zip(x.chunks_exact(4)) {
-            // ... and copy each of them to output, leaving out the A byte
-            output.copy_from_slice(&chunk[0..3]);
-        }
-        println!("Converted image: {:?}", time.elapsed());
-        // Convert x into ndarray::ArrayBase<ndarray::CowRepr<'_, f32>,
+            let mut letterboxed: ImageBuffer<image::Rgb<f32>, Vec<f32>> =
+                image::ImageBuffer::new(self.input_size.0, self.input_size.1);
 
-        let time = std::time::Instant::now();
-        println!("Converting image to tensor...");
-        let array: ndarray::ArrayBase<ndarray::CowRepr<'_, f32>, ndarray::Dim<ndarray::IxDynImpl>> =
-            CowArray::from(ndarray::Array::from_shape_vec(
+            let background = image::Rgb([0.015686275, 0.015686275, 0.015686275]); // Convert background color values to f32
+            for pixel in letterboxed.pixels_mut() {
+                *pixel = background;
+            }
+
+            let width_w_ratio = self.input_size.0 as f32 / original_image_width as f32;
+            let height_w_ratio = self.input_size.1 as f32 / original_image_height as f32;
+
+            let ratio = width_w_ratio.min(height_w_ratio);
+            let new_width = (original_image_width as f32 * ratio) as u32;
+            let new_height = (original_image_height as f32 * ratio) as u32;
+
+            let image_resized =
+                original_image.resize(new_width, new_height, image::imageops::FilterType::Nearest);
+
+            // Swap R and B channels
+            let image_resized: RgbImage =
+                ImageBuffer::from_fn(image_resized.width(), image_resized.height(), |x, y| {
+                    let pixel = image_resized.get_pixel(x, y);
+                    image::Rgb([pixel[2], pixel[1], pixel[0]])
+                });
+
+            let x = (self.input_size.0 - image_resized.width()) / 2;
+            let y = (self.input_size.1 - image_resized.height()) / 2;
+
+            let image_resized_f32: ImageBuffer<image::Rgb<f32>, Vec<_>> = image_resized.convert();
+            image::imageops::overlay(&mut letterboxed, &image_resized_f32, x.into(), y.into());
+
+            let lettboxed_to_u8: RgbaImage = letterboxed.convert();
+            lettboxed_to_u8.save("letterboxed.jpg").unwrap();
+
+            // Convert the image to a tensor
+            let t = CowArray::from(ndarray::Array::from_shape_vec(
                 (1, 3, self.input_size.0 as usize, self.input_size.1 as usize),
-                output.as_raw().iter().map(|x| *x as f32).collect(),
+                letterboxed.as_raw().to_vec(),
             )?)
-            .into_dyn();
-
-        // Pretend it's a batch of 12 by repeating the first image 12 times
-        let array = array
-            .broadcast((1, 3, self.input_size.0 as usize, self.input_size.1 as usize))
-            .unwrap()
             .to_owned();
-        println!("Converted image to tensor: {:?}", time.elapsed());
 
-        // convert image into a tensor
-        // let array = ndarray::Array::from_shape_vec(
-        //     (1, 3, 640, 640),
-        //     vec![0.0; 1 * 3 * 640 * 640],
-        // )?;
-        // let tensor = ndarray_tensor(&array, self.environment.clone())?;
+            println!("input shape = {:?}", t.shape());
 
-        // let array: ndarray::ArrayBase<ndarray::CowRepr<'_, f32>, ndarray::Dim<ndarray::IxDynImpl>> =
-        //     CowArray::from(ndarray::Array::from_shape_vec(
-        //         (1, 3, 640, 640),
-        //         vec![0.0; 1 * 3 * 640 * 640],
-        //     )?)
-        //     .into_dyn();
+            // Change the image to be BGR instead of RGB. It has the shape [1, 3, 640, 640] so we need to reverese the channels in the second axis
+            // let mut t = t;
+            // t.raw_view_mut().swap_axes(1, 2);
+
+            println!("input = {:?}", t);
+
+            t
+        };
 
         let time = std::time::Instant::now();
         println!("Running model...");
-        let inputs = ort::inputs![
-            array, // Pass the CowRepr array reference here
-        ]
-        .unwrap();
+        let binding = self.session.run(ort::inputs![image].unwrap()).unwrap();
+        let binding = binding[0].extract_tensor().unwrap();
+        let outputs = &binding.view();
 
-        let outputs = &self.session.run(inputs).unwrap()[0];
-        let output = outputs.extract_tensor::<f32>()?;
         println!("Ran model: {:?}", time.elapsed());
 
         let time = std::time::Instant::now();
-        println!("Processing output...");
-        let view = output.view();
+        println!("Processing model output...");
 
-        let mut detections = vec![];
+        let rows = *outputs.shape().get(1).unwrap();
+        println!("rows: {:?}", rows);
+        let mut detections = Vec::<YoloDetection>::with_capacity(rows);
 
-        for i in 0..25500 {
-            let x = view[[0, i, 0]];
-            let y = view[[0, i, 1]];
-            let width = view[[0, i, 2]];
-            let height = view[[0, i, 3]];
-            let class_index = view[[0, i, 4]];
-            let confidence = view[[0, i, 5]];
+        let mut overall_max_confidence: f32 = 0.0;
 
-            if confidence > confidence_threshold {
-                detections.push(YoloDetection {
-                    x,
-                    y,
-                    width,
-                    height,
-                    class_index: class_index as u32,
-                    confidence,
-                });
+        for row in 0..rows {
+            let cx: &f32 = &outputs[[0, row, 0]];
+            let cy: &f32 = &outputs[[0, row, 1]];
+            let w: &f32 = &outputs[[0, row, 2]];
+            let h: &f32 = &outputs[[0, row, 3]];
+            let sc: &f32 = &outputs[[0, row, 4]];
+
+            // println!("cx: {:?}, cy: {:?}, w: {:?}, h: {:?}, sc: {:?}", cx, cy, w, h, sc);
+
+            overall_max_confidence = overall_max_confidence.max(*sc);
+
+            let mut x_min = *cx - *w / 2.0;
+            let mut y_min = *cy - *h / 2.0;
+
+            x_min /= self.input_size.0 as f32;
+            y_min /= self.input_size.1 as f32;
+            let mut width = *w / self.input_size.0 as f32;
+            let mut height = *h / self.input_size.1 as f32;
+
+            x_min = x_min.max(0.0).min(1_f32);
+            y_min = y_min.max(0.0).min(1_f32);
+            width = width.max(0.0).min(1_f32);
+            height = height.max(0.0).min(1_f32);
+
+            let mat_size = outputs.shape();
+            let classes = *mat_size.get(2).unwrap() - 5;
+            let mut classes_confidences = vec![];
+
+            for j in 5..5 + classes {
+                let confidence: &f32 = &outputs[[0, row, j]];
+                classes_confidences.push(confidence);
             }
+
+            let mut max_index = 0;
+            let mut max_confidence = 0.0;
+            for (index, confidence) in classes_confidences.iter().enumerate() {
+                if *confidence > &max_confidence {
+                    max_index = index;
+                    max_confidence = **confidence;
+                }
+            }
+
+            detections.push(YoloDetection {
+                x: x_min,
+                y: y_min,
+                width,
+                height,
+                class_index: max_index as u32,
+                confidence: *sc,
+            })
         }
-        println!("Processed output: {:?}", time.elapsed());
+
+        println!("Overall max confidence: {:?}", overall_max_confidence);
+
+        println!("Processed model output: {:?}", time.elapsed());
+        println!("Detections: {:?}", detections.len());
 
         let time = std::time::Instant::now();
         println!("Filtering detections...");
         let detections = filter_confidence(detections, confidence_threshold);
-        println!("Filtered detections: {:?}", time.elapsed());
+        println!(
+            "Filtered detections: {:?} {}",
+            time.elapsed(),
+            detections.len()
+        );
 
         let time = std::time::Instant::now();
         println!("Non-maximum suppression...");
         let detections = non_max_suppression(detections, iou_threshold);
-        println!("Non-maximum suppression: {:?}", time.elapsed());
+        println!(
+            "Non-maximum suppression: {:?} {}",
+            time.elapsed(),
+            detections.len()
+        );
 
         let detections = YoloImageDetections {
             file: image_path.to_string(),
-            image_width: image.width(),
-            image_height: image.height(),
+            image_width: original_image_width,
+            image_height: original_image_height,
             detections,
         };
 
@@ -229,75 +275,31 @@ fn filter_confidence(detections: Vec<YoloDetection>, min_confidence: f32) -> Vec
         .collect()
 }
 
-// fn convert_to_detections(outputs: &Mat) -> Result<Vec<YoloDetection>, Error> {
-//     let rows = *outputs.mat_size().get(1).unwrap();
-//     let mut detections = Vec::<YoloDetection>::with_capacity(rows as usize);
-
-//     for row in 0..rows {
-//         let cx: &f32 = outputs.at_3d(0, row, 0)?;
-//         let cy: &f32 = outputs.at_3d(0, row, 1)?;
-//         let w: &f32 = outputs.at_3d(0, row, 2)?;
-//         let h: &f32 = outputs.at_3d(0, row, 3)?;
-//         let sc: &f32 = outputs.at_3d(0, row, 4)?;
-
-//         let mut x_min = *cx - *w / 2.0;
-//         let mut y_min = *cy - *h / 2.0;
-
-//         x_min /= self.input_size.width as f32;
-//         y_min /= self.input_size.height as f32;
-//         let mut width = *w / self.input_size.width as f32;
-//         let mut height = *h / self.input_size.height as f32;
-
-//         x_min = x_min.max(0.0).min(1_f32);
-//         y_min = y_min.max(0.0).min(1_f32);
-//         width = width.max(0.0).min(1_f32);
-//         height = height.max(0.0).min(1_f32);
-
-//         let mat_size = outputs.mat_size();
-//         let classes = *mat_size.get(2).unwrap() - 5;
-//         let mut classes_confidences = vec![];
-
-//         for j in 5..5 + classes {
-//             let confidence: &f32 = outputs.at_3d(0, row, j)?;
-//             classes_confidences.push(confidence);
-//         }
-
-//         let mut max_index = 0;
-//         let mut max_confidence = 0.0;
-//         for (index, confidence) in classes_confidences.iter().enumerate() {
-//             if *confidence > &max_confidence {
-//                 max_index = index;
-//                 max_confidence = **confidence;
-//             }
-//         }
-
-//         detections.push(YoloDetection {
-//             x: x_min,
-//             y: y_min,
-//             width,
-//             height,
-//             class_index: max_index as u32,
-//             confidence: *sc,
-//         })
-//     }
-
-//     Ok(detections)
-// }
-
 #[cfg(test)]
 mod test {
+    use crate::yolov5::helpers::render_detections;
+
     use super::*;
     use tracing_test::traced_test;
 
     #[traced_test]
     #[test]
     fn test_model() {
-        let mut model = YoloModel::new_from_file("../md_v5a.0.0-dynamic.onnx", (640, 640)).unwrap();
+        let mut model = YoloModel::new_from_file("../md_v5a.0.0-640x640.onnx", (640, 640)).unwrap();
 
         let detections = model
-            .detect("./tests/fixtures/dataset/IMG_0089_peccary.JPG", 0.1, 0.45)
+            .detect("./tests/fixtures/dataset/IMG_0089_peccary.JPG", 0.001, 0.45)
             .unwrap();
 
         println!("{:?}", detections);
+
+        println!("Detections: {:?}", detections.detections.len());
+
+        render_detections(
+            "./tests/fixtures/dataset/IMG_0089_peccary.JPG",
+            &detections,
+            "tmp.jpg",
+        )
+        .unwrap();
     }
 }
