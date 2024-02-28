ac714a0aacb8af6eb1b440a11d71875c585da7ce
Ben Evans <ben@bluechimp.io>
2024-02-28 12:42:33 +0000

    Remove old CUDA linking

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,13 +1,4 @@
 fn main() {
-    // if cfg!(feature = "cuda") {
-    //     // let cuda_path = std::env::var("CUDA_PATH").expect("CUDA_PATH not set");
-    //     // println!("cargo:rustc-link-search=native={}\\lib\\x64", cuda_path);
-    //     println!("cargo:rustc-link-lib=cudart_static");
-    //     println!("cargo:rustc-link-lib=cublas");
-    //     println!("cargo:rustc-link-lib=cublasLt");
-    //     println!("cargo:rustc-link-lib=cudnn");
-    // }
-
     if cfg!(target_os = "macos") {
         println!("cargo:rustc-link-lib=framework=OpenCL");
         println!("cargo:rustc-link-lib=framework=Accelerate");
