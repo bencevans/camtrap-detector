fb70d08837b9a4a85457178a13095243887ce01b
Ben Evans <Benjamin.Evans@ioz.ac.uk>
2026-06-25 10:39:03 +0100

    Fix ESLint parser options

--- eslint.config.js
+++ eslint.config.js
@@ -13,7 +13,6 @@ export default tseslint.config(
           allowDefaultProject: ["*.js", "src/vite.config.js"],
         },
         tsconfigRootDir: import.meta.dirname,
-        project: "./tsconfig.json",
       },
     },
   },
